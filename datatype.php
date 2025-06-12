3abe482fd235db3eb395aefea04b9609af8dd3ed
Pranavraj09 <pranavraj0909@gmail.com>
2025-06-12 14:56:19 +0530

    first commit

---
+++ datatype.php
@@ -0,0 +1,14 @@
+<?php
+$x = "hello bhai";
+$x = 10; // Reassigning $x to an integer
+$x = "10.5"; // Reassigning $x to a string with help of a inveted code
+$x = 10.5; // Reassigning $x to a float
+$x = true; // Reassigning $x to a boolean
+$x = null; // Reassigning $x to null
+$x = array(1, 2, 3); // Reassigning $x to an array
+echo $x . "<br>";
+var_dump($x); // Outputs the type and value of $x
+?>
+
+
+
---
+++ hello.php
@@ -0,0 +1,30 @@
+<?php
+echo("Hello, World!<br>");
+?>
+<!--variable use-->
+<?php
+echo "Hello, World!<br>";
+// Variable declaration and output
+$a="hello php";
+echo($a);
+?>
+<?php
+$age = 25;
+echo "I am $age years old.<br>";
+// Using variables in strings
+$greeting = "Hello";
+$name = "Alice";
+echo "$greeting, $name!<br>";
+// Concatenation example
+$firstName = "John";
+$lastName = "Doe";
+echo "My name is " . $firstName . " " . $lastName . ".<br>";
+// Using variables in a sentence
+$city = "New York";
+$country = "USA";
+echo "I live in $city, $country.<br>";
+// Variable in a mathematical operation
+$number = 10;
+$double = $number * 2;
+echo "Double of $number is $double.<br>";
+?>
