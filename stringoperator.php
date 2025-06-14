32bea8196d8c02e9b5b83ccad3c739fcb2aa5c1f
Pranavraj09 <pranavraj0909@gmail.com>
2025-06-14 22:05:05 +0530

    commit while loop

---
+++ stringoperator.php
@@ -0,0 +1,11 @@
+<!-- <?php
+$a="hello";
+$b=$a ."world!";
+echo $b;
+?> -->
+<?php
+$a="hello ";
+$a .="this is my";
+$a .=" world!";
+echo $a;
+?>
---
+++ ternaryoperator.php
@@ -0,0 +1,5 @@
+<?php
+$x = 30;
+$z = "value is: " . ($x > 20 ? "greater than 20" : "less than or equal to 20") . "<br>";
+echo $z;
+?>
