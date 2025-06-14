c6c9f0ff6a26da64a31ccd3d674a8fcf710876b5
Pranavraj09 <pranavraj0909@gmail.com>
2025-06-14 16:10:40 +0530

    if commit

--- if.php
+++ if.php
@@ -1,11 +1,40 @@
 <?php
-$a=10;
-$b=18;
-if($a < $b){
-    echo ("$a is less than $b <br>");
-}
-echo "all are ohkk"
+// $a=10;
+// $b=18;
+// if($a < $b){
+//     echo ("$a is less than $b <br>");
+// }
+// echo "all are ohkk"
 // if($a > $b):
 //     echo ("$a is greater than $b");
 // endif
+// if and else statement
+$a=14;
+if($a>=18){
+    echo("yo are eligible to vote<br>");
+}else{
+    echo("you are not eligible to vote<br>");
+}
 ?>
+<?php
+$name = "John";
+$gender = "male";
+if ($gender == "male"){
+    echo "Hello Mr. $name<br>";
+}
+else {
+    echo "Hello Ms. $name<br>";
+}
+?>
+<!-- if elseif -->
+ <?php
+ $a =29;
+ if($a <18){
+    echo("you are minor<br>");
+ }else if($a == 18){
+    echo("you are eligible to vote<br>");
+}
+else {
+    echo("you are major<br>");
+ }
+ ?>
